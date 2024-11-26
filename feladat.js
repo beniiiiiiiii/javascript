3cfe38e1578721d690dabb8308a3f7f94225a18b
beniiiiiiiii <117970056+beniiiiiiiii@users.noreply.github.com>
2024-11-26 14:56:19 +0100

    fajlkezeles feladat

---
+++ feladat.js
@@ -0,0 +1,34 @@
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const filePath = path.join(__dirname, "adatok.json")
+
+const users = [
+    {id: 1, name: 'Adam'}, {id: 2, name: 'Bianka'}, {id: 3, name: 'Cecil'}
+]
+
+function readFile() {
+    let content = '';
+
+    try{
+        content = fs.readFileSync(filePath, 'utf-8')
+    } catch (err){
+        console.log(err)
+    }
+    console.log(content)
+    
+    const newdata = JSON.parse(content)
+    console.log(newdata)
+}
+
+function writeFile() {
+    try{
+        fs.writeFileSync(filePath, JSON.stringify(users))
+    } catch (err){
+        console.log(err)
+    }
+}
+
+readFile()
