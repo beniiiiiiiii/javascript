2d251722a8a33fbe4d3dccd89322f5cc5b3a8212
Midvincible <117970056+beniiiiiiiii@users.noreply.github.com>
2025-09-25 11:32:48 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,64 +1,52 @@
 import express from "express";
+import * as db from './data/db.js'
+import { json } from "body-parser";
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.json())
+app.use(express.json());
 
-const cars = [
-  { id: 1, brand: "bmw", model: "sedam" },
-  { id: 1, brand: "kia", model: "sportage" },
-  { id: 1, brand: "toyota", model: "supra" },
-  { id: 1, brand: "nissan", model: "gtr" },
-  { id: 1, brand: "audi", model: "r8" },
-];
-
-//getall
-app.get('/cars', (req, res) => {
-    res.status(200).json(cars);
-})
-
-//get by id
-app.get('/cars/:id', (req, res) => {
-    const id = req.params.id;
-    const car = cars.find(car => car.id === id);
+app.get("/cars", (req, res => {
+    const cars = db.getAllCars()
+    res.statu(200).json(cars)
+}))
+app.get("/cars/:id", (req, res => {
+    const car = db.getCarById(+req.params.id)
     if(!car){
-        res.status(404).json({ message : "Car not found"})
+        return res.status(404).json({message: "car not found"});
     }
-    res.status(200).json(car)
-})
-
-//post
-app.post('/cars', (req, res) => {
-    const {brand , model} = req.body;
+    res.status(200).json(car);
+}))
+app.post("/cars", (req, res => {
+    const {brand, model} = req.body;
     if(!brand || !model){
-        res.status(400).json({ message: "Invalid creds"})
+        return res.status(404),json({message: "not found"})
     }
-    const id = cars.length ? cars[cars.length -1].id + 1 : 1;
-    const car = {id, brand, model}
-    cars.push(car) 
-    res.status(200).json(car)
-})
-
-//put
-app.put('/cars/:id', (req, res) => {
-    const id = Number(req.params.id);
-    let car = cars.find(car => car.id === id);
+    const saved = db.saveCar(brand, model)
+    const car = db.getCarById(saved.lastInsertRowid)
+    res.status(200).json(car);
+}))
+app.put("/cars", (req, res => {
+    const id = req.params.id
+    let car = db.getCarById(id);
     if(!car){
-        res.status(404).json({ message: "car not found"})
+        return res.status(404),json({message: "not found"})
     }
     const {brand, model} = req.body;
-    if(!brand || ! model){
-        res.status(400).json({ message: "invalid creds"})
+    if(!brand || !model){
+        return res.status(400),json({message: "invalid"})
     }
-    const index = cars.indexOf(car)
-    car = {
-        id: user.id,
-        brand: brand,
-        model: model
-    }
-    cars[index] = car;
-    res.status(200).json(car)    
-})
+    const updated = db.saveCar(brand, model)
+    res.status(200).json(car);
+}))
+app.delete("/cars/:id", (req, res => {
+    db.deleteCar(+req.params.id)
+    res.status(200).json("success")
+}))
 
-//delete
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
